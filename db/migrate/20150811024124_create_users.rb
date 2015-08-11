1a2efa98d5327110b5d979bf3504ca40d2467835
Wes Hargrove <wesleyhargrove@gmail.com>
2015-08-10 19:48:18 -0700

    Fix syntax error in user migration

--- db/migrate/20150811024124_create_users.rb
+++ db/migrate/20150811024124_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.String :first_name null: false
-      t.String :last_name null: false
-      t.String :email null: false
-      t.String :password_digest null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
 
       t.timestamps null: false
     end
