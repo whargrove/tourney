d4a919e3dfaa286ea21bbefd91baf8828e9685ca
Wes Hargrove <wesleyhargrove@gmail.com>
2015-08-10 22:45:03 -0700

    Add comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
 
   root 'users#index' # TODO: This is only temporary
 
+  # Registration
   get 'signup', to: 'users#new', as: 'signup'
+
+  # Authentication
   get 'login', to: 'sessions#new', as: 'login'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 
+  # Users
   get 'users', to: 'users#index'
   post 'users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
