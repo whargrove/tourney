97919af486875beb40d86935d2df877be41482b9
Wes Hargrove <wesleyhargrove@gmail.com>
2015-08-10 22:44:34 -0700

    Remove users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root 'users#index' # TODO: This is only temporary
 
   get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :users
+  get 'users', to: 'users#index'
+  post 'users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 
 end
